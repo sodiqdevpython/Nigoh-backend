3b453d1317dd6adf0e780e7a7c89c998f40de1fb
Sodiq Oydinov <sodiqdevpython@gmail.com>
2026-05-04 15:06:44 +0500

    config o'zgardi

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ WORKDIR /app
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+# Pip uchun global sozlamalar (Progress bar va keshni butunlay o'chirish)
+ENV PIP_PROGRESS_BAR=off
+ENV PIP_NO_CACHE_DIR=1
+
 # Kutubxonalarni o'rnatish
 COPY requirements.txt /app/
 RUN pip install --upgrade pip && pip install -r requirements.txt
--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,9 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
