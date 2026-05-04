42bf8f080861bbadf8d3ac62da0225d58c991057
Sodiq Oydinov <sodiqdevpython@gmail.com>
2026-05-04 15:31:49 +0500

    env to'g'irlandi2

--- config/asgi.py
+++ config/asgi.py
@@ -1,10 +1,17 @@
 import os
+import django # <--- Qo'shildi
 from django.core.asgi import get_asgi_application
+
+# Sozlamalarni o'rnatamiz
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Djangoni to'liq ishga tushiramiz (Importlardan oldin bo'lishi shart!)
+django.setup() 
+
+# Endi qolgan narsalarni import qilsak bo'ladi
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import endpoints.routing  # Routerni import qilamiz
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import endpoints.routing 
 
 django_asgi_app = get_asgi_application()
 
